cbfa372c8ee7c4cae648517aa01b376bb01a9a36
pato <nerojapan@gmail.com>
2023-02-19 15:20:43 +0900

    Initial commit

--- calc.js
+++ calc.js
@@ -4,31 +4,19 @@ var arrayTest = new Array(10);
 //var arrayTest2 = new Array("ans_no0","ans_no1","ans_no2","ans_no3","ans_no4","ans_no5","ans_no6","ans_no7","ans_no8","ans_no9");
 var arrayTest2 = [
                    [ "fm_no0", "ans_no0" ],
-                   [ "fm_no1", "ans_no1" ],
-                   [ "fm_no2", "ans_no2" ],
-                   [ "fm_no3", "ans_no3" ],
-                   [ "fm_no4", "ans_no4" ],
-                   [ "fm_no5", "ans_no5" ],
-                   [ "fm_no6", "ans_no6" ],
-                   [ "fm_no7", "ans_no7" ],
-                   [ "fm_no8", "ans_no8" ],
-                   [ "fm_no9", "ans_no9" ],
+//                   [ "fm_no1", "ans_no1" ],
+//                   [ "fm_no2", "ans_no2" ],
+//                   [ "fm_no3", "ans_no3" ],
+//                   [ "fm_no4", "ans_no4" ],
+//                   [ "fm_no5", "ans_no5" ],
+//                   [ "fm_no6", "ans_no6" ],
+//                   [ "fm_no7", "ans_no7" ],
+//                   [ "fm_no8", "ans_no8" ],
+//                   [ "fm_no9", "ans_no9" ],
 ];
 
 
-var arrayTest3 = [
-                   [ "これはリンゴです.....&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;", "This is an apple." ],
-                   [ "彼は僕の兄です", "He is my brother." ],
-                   [ "これは何ですか？", "What is this?" ],
-                   [ "fm_no3", "ans_no3" ],
-                   [ "fm_no4", "ans_no4" ],
-                   [ "fm_no5", "ans_no5" ],
-                   [ "fm_no6", "ans_no6" ],
-                   [ "fm_no7", "ans_no7" ],
-                   [ "fm_no8", "ans_no8" ],
-                   [ "fm_no9", "ans_no9" ],
-];
-
+let num1 = 0;
 
 
 // ------------------------------------------------------------------------
@@ -43,17 +31,17 @@ function init()
 
 function updateFormula() 
 {
-  for (let i = 0; i < arrayTest2.length; i++){
-      document.getElementById(arrayTest2[i][0]).innerHTML = get_test2(i);
-  }
+//  for (let i = 0; i < arrayTest2.length; i++){
+      document.getElementById(arrayTest2[0][0]).innerHTML = get_test2(0);
+//  }
 }
 
 function updateAnswer() 
 {
   var str = "?????"
-  for (let i = 0; i < arrayTest2.length; i++){
-      document.getElementById(arrayTest2[i][1]).innerHTML = str;
-  }
+//  for (let i = 0; i < arrayTest2.length; i++){
+      document.getElementById(arrayTest2[0][1]).innerHTML = str;
+//  }
 }
 
 // ------------------------------------------------------------------------
@@ -66,11 +54,11 @@ function get_test2(index)
 //    var num2=0
 //    var num3 = 0;
 //
-//    var min = Math.ceil(-9);
-//    var max = Math.floor(9);
+    var min = 0;
+    var max = arrayTest3.length;
 //  
 //    while (num1==0) {
-//        num1= Math.floor(Math.random() * (max - min) + min);
+        num1= Math.floor(Math.random() * (max - min) + min);
 //    }
 //
 //    min = 1;
@@ -90,14 +78,20 @@ function get_test2(index)
 //    }
 //    arrayTest[index]=num3;
 
-    return arrayTest3[index][0];
+	// 単語の配列のlenの中から、ランダムな数字を出す
+	// その数字を覚えておく
+	// 
+
+
+
+    return arrayTest3[num1][0];
 }
 
 // 指定されたNoの答えを、問題作成時に作った配列から取得
 function get_ans2(num){
 //    var str = `x = ${ arrayTest[num]}`;
 	
-	return arrayTest3[num][1];
+	return arrayTest3[num1][1];
 }
 
 
---
+++ word.js
@@ -0,0 +1,15 @@
+
+var arrayTest3 = [
+[ "０００００００１.....&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" , "ans０００００００１" ],
+[ "０００００００２.....&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" , "ans０００００００２" ],
+[ "０００００００３.....&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" , "ans０００００００３" ],
+[ "０００００００４.....&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" , "ans０００００００４" ],
+[ "０００００００５.....&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" , "ans０００００００５" ],
+[ "０００００００６.....&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" , "ans０００００００６"  ],
+[ "０００００００７.....&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" , "ans０００００００７" ],
+[ "０００００００８.....&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" , "ans０００００００８" ],
+[ "０００００００９.....&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" , "ans０００００００９" ],
+[ "００００００１０.....&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" , "ans００００００１０" ],
+];
+
+
