f099f0b22dd16774a94073e0e3e18d594696778c
pato <nerojapan@gmail.com>
2023-02-19 23:02:55 +0900

    test

--- word.js
+++ word.js
@@ -1,15 +1,55 @@
 
 var arrayTest3 = [
-[ "０００００００１.....&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" , "ans０００００００１" ],
-[ "０００００００２.....&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" , "ans０００００００２" ],
-[ "０００００００３.....&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" , "ans０００００００３" ],
-[ "０００００００４.....&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" , "ans０００００００４" ],
-[ "０００００００５.....&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" , "ans０００００００５" ],
-[ "０００００００６.....&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" , "ans０００００００６"  ],
-[ "０００００００７.....&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" , "ans０００００００７" ],
-[ "０００００００８.....&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" , "ans０００００００８" ],
-[ "０００００００９.....&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" , "ans０００００００９" ],
-[ "００００００１０.....&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" , "ans００００００１０" ],
+
+[ "これは良い本です。" , "This is a good book. " ],
+[ "この辞書は良い。 " , "This dictionary is good. " ],
+[ "あれはおもしろい本ですか？ はい、そうです。" , " Is that an interesting book? Yes, it is." ], 
+[ "あの本はおもしろい です か？ いえ、おもしろくありませ ん。" , " Is that book interesting? No, it isnt." ], 
+[ "これは正しくない。" , "This is not right." ],
+
+[ "あれは本物の花ではない。" , " That is not a real flower." ],
+[ "このスープはあまり美味しくない。 " , "This soup is not very tasty." ],
+[ "これは塩ですか、それとも砂糖ですか？ 砂糖 です。" , "Is this salt or sugar? It is sugar." ],
+[ "あの女性はフランス人ですか、イタリア人ですか？ フランス人です。&nbsp;&nbsp;&nbsp;" , "Is that woman French or Italian? She is French." ],
+[ "あの男性は日本人ですか、それとも中国人ですか？ 日本人 です。" , "Is that man Japanese or Chinese? He is Japanese." ],
+
+[ "これらは子供向けの本です。" , "These are books for children." ],
+[ "あれらは本当の花ですか？ いいえ、違います。 " , "Are those real flower?  No, they aren't." ],
+[ "この少年たちは中学生です。" , "These boys are junior high school students." ],
+[ "あの少女たちは高校生ですか？ はい、そうです。 " , "Are those girls high school students?  Yes,they are." ],
+[ "これらの車はとても高い。" , "These cars are very expensive." ],
+[ "あれらの家は新しくない。" , "Those houses are not new." ],
+[ "これらは本ですか、それとも雑誌 ですか？ 雑誌 です。 " , "Are these books or magazines? They are magazines." ],
+[ "あれらの車は新しいですか、それとも古いです か？ 新しいです。 " , "Are those cars new or old? They are new." ],
+[ "この男性たちはアメリカ人ですか、それともドイツ人です か？ ドイツ人です。 " , "Are these men American or German? They are German." ],
+[ "あの女性たちは日本人ですか、それとも韓国人ですか？ 韓国 人 です。" , "Are those women Japanese or Korean? They are Korean." ],
+
+[ "これは何ですか？ カメラです。" , "What is this? It is a camera." ],
+[ "あれは何ですか？ 灰皿です。" , "What is that? It is an ashtray." ],
+[ "これは何ですか？ 置時計です。" , "What is this? It is a clock." ],
+[ "あれは何ですか？ 枕です。" , "What is that? It is a pillow." ],
+[ "これらは何ですか？ フランスの雑誌です。" , "What are these? They are French magazines." ],
+[ "あの鳥たちは何ですか？ スズメです。" , "What are those birds? They are sparrow." ],
+[ "あれらは何ですか？ 古い切手です。" , "What are those?  They are old stamps." ],
+[ "あの男の人たちは何ですか？ 警官です。" , "What are those men? They are policemen." ],
+[ "あの女性たちは何ですか？ 秘書です。" , "What are those women? They are secretaries." ],
+[ "あの人たちは何ですか？ 弁護士です。" , "What are those people? They are lawyers." ],
 ];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
